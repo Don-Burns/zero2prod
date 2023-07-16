17f0df1d3b544bc3a801dc01ce4ec7ed8d134886
Donal Burns <donal.w.burns@hotmail.com>
2023-07-16 13:52:34 +0100

    Add tracing

---
+++ src/telemetry.rs
@@ -0,0 +1,34 @@
+use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+/// function to build out the tracing subscriber
+/// name is the name of the sub
+/// env_filter will filter to info, debug etc.
+/// sink is where to send the tracing output e.g. stdout
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    // set up tracing formatting etc.
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    // set up log tracer to redirect normal logs through tracing
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set tracing subscriber");
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,23 @@
+use once_cell::sync::Lazy;
 use sqlx::{types::Uuid, Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    // Since the tracing is spawned in all invocations of `spawn_app`
+    // and this is not allowed by tracing within a single application
+    // need to force only once execution
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -8,6 +25,9 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // logging / tracing
+    Lazy::force(&TRACING);
+    // app work
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let mut configuration = get_configuration().expect("Failed to read configuration.");
